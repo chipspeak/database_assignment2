4b9f59032fffdc06ea0ecb4105d7f225097f2b2c
Pat Walsh-O'Connor <114754356+chipspeak@users.noreply.github.com>
2023-12-15 15:40:39 +0000

    Assignment Complete
    All sections complete and file has been reformatted for legibility. If any issues persist in executing in VS code, convert each query into a single line via VS code formatting options.

--- database_assignment2_patrick_o_connor_20040412.js
+++ database_assignment2_patrick_o_connor_20040412.js
@@ -1,4 +1,11 @@
-//---Queries---
+/*
+Student Name: Patrick O'Connor
+Student ID: 20040412
+gitHub repo: https://github.com/chipspeak/database_assignment2
+*/
+
+//Part 1 ---Queries----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 //1. Horror films from 1980 to present that are less than an R rating and have an imdb rating of 7.0 or above.
 db.movies
@@ -25,6 +32,7 @@ db.movies
   .limit(10)
   .pretty();
 
+
 //2. Romantic comedies starring Adam Sandler with an rotten tomatoes viewer rating of 3 or above.
 db.movies
   .find(
@@ -46,6 +54,7 @@ db.movies
   .sort({ "tomatoes.viewer.rating": -1 })
   .limit(20)
   .pretty();
+
 
 //3. Multi-Award winning documentaries with a run time of less than 2 hours (regex to match the word wins for awards).
 db.movies
@@ -71,14 +80,18 @@ db.movies
   .limit(10)
   .pretty();
 
+
 //4. A count of films directed by Steven Spielberg and starring Tom Hanks from 2010 onwards
 db.movies
   .find({
-    directors: { $in: ["Steven Spielberg"] },
-    cast: { $in: ["Tom Hanks"] },
-    year: { $gte: 2010 },
+    $and: [
+      { directors: "Steven Spielberg" },
+      { cast: { $elemMatch: { $eq: "Tom Hanks" } } },
+      { year: { $gte: 2010 } },
+    ],
   })
   .count();
+
 
 //5. Highest rated items on IMDB from the 1990s excluding documentaries
 db.movies
@@ -96,6 +109,7 @@ db.movies
   .limit(5)
   .pretty();
 
+
 //6. A count of the amount of War films based upon a novel that appear in the database that were made prior to 1966 with a runtime of over 2 hours
 db.movies
   .find({
@@ -106,7 +120,140 @@ db.movies
   })
   .count();
 
-//---Aggregate pipelines
+
+
+//Part 2 ---Inserts--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//(films)
+//insertMany is used to insert Dune(2021) and Joker(2019)
+db.movies.insertMany([
+  {
+    _id: 611991,
+    title: "Dune",
+    year: 2021,
+    runtime: 155,
+    directors: ["Denis Villeneuve"],
+    cast: ["Timothée Chalamet", "Rebecca Ferguson", "Zendaya"],
+    plot: "Set in the far distant future, in a post-artificial intelligence universe, the film follows Paul Atreides as his family, the noble House Atreides, is thrust into a war for the deadly and inhospitable desert planet Arrakis",
+    genres: ["Sci-Fi", "Drama"],
+    imdb: { rating: 8.0, votes: 732664 },
+  },
+  {
+    _id: 611992,
+    title: "Joker",
+    year: 2019,
+    runtime: 122,
+    directors: ["Todd Phillips"],
+    cast: ["Joaquin Phoenix", "Robert De Niro", "Zazie Beetz"],
+    plot: "Set in 1981, it follows Arthur Fleck, a failed clown and aspiring stand-up comic whose descent into mental illness and nihilism inspires a violent countercultural revolution against the wealthy in a decaying Gotham City.",
+    genres: ["Thriller", "Psychological"],
+    imdb: { rating: 8.4, votes: 1400000 },
+  },
+]);
+//find to check for Dune after insertion
+db.movies.find({ _id: 611991 });
+//find to check for Joker after insertion
+db.movies.find({ _id: 611992 });
+
+
+//(users)
+//insertMany is used to insert 3 user documents with an array of favourites for each user alongside typical properties like email and password
+db.users.insertMany([
+  {
+    _id: 611993,
+    name: "Pat O Connor",
+    email: "pat@email.com",
+    password: "itsAsecret",
+    favourites: [
+      ObjectId("573a13bff29313caabd5f799"),
+      ObjectId("573a13cdf29313caabd844c7"),
+    ],
+  },
+  {
+    _id: 611994,
+    name: "Homer Simpson",
+    email: "homerS@springfieldPowerPlant.org",
+    password: "password",
+    favourites: [
+      ObjectId("573a1396f29313caabce3c3e"),
+      ObjectId("573a1399f29313caabcec9e8"),
+    ],
+  },
+  {
+    _id: 611995,
+    name: "Dougal MacGuire",
+    email: "inept@priests.ie",
+    password: "1234",
+    favourites: [
+      ObjectId("573a1398f29313caabce9d81"),
+      ObjectId("573a1398f29313caabcea86a"),
+    ],
+  },
+]);
+//check for user upon creation
+db.users.find({ _id: 611993 });
+
+
+
+//Part 3 ---Update,Delete------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//(updates)
+
+//updates Dune's imdb score to 8.1 and increases its imdb votes by 127
+db.movies.updateOne(
+  { _id: 611991 },
+  { $set: { "imdb.rating": 8.5 }, $inc: { "imdb.votes": 1 } }
+);
+//find to check for Dune after update
+db.movies.find({ _id: 611991 });
+
+
+//updates Pat's favourites with Dune as an entry
+db.users.updateOne({ _id: 611993 }, { $push: { favourites: 611991 } });
+//find to check push was successful
+db.users.find({ _id: 611993 });
+
+
+//updates Joker with an awards field
+db.movies.updateOne(
+  { _id: 611992 },
+  {
+    $set: {
+      awards:
+        "Won 2 Oscars. Best Actor for Jaoquin Phoenix and Best Original Score for Hildur Gudnadottir",
+    },
+  }
+);
+//find to check addition of awards field was successful
+db.movies.find({ _id: 611992 });
+
+
+//updates Dune with a writers field
+db.movies.updateOne(
+  { _id: 611991 },
+  {
+    $set: {
+      writers: [
+        "Frank Herbert(Novel)",
+        "Denis Villeneuve",
+        "Jon Spaihts",
+        "Eric Roth",
+      ],
+    },
+  }
+);
+//find to check for Dune after update
+db.movies.find({ _id: 611991 });
+
+
+//(delete)
+//deleting Dune
+db.movies.deleteOne({ _id: 611991 });
+//find to check for Dune after delete
+db.movies.find({ _id: 611991 });
+
+
+
+//Part 4 ---Aggregate pipelines----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 //1. --- Films where the director also stars sorted by number of films in which they appear and direct. Also features the range of years in which these films occur.
 /*
